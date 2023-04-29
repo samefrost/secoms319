6aba6e8c0ab3ffc2dade18cd648aa78283128ac7
kerwnshaw <kcshaw2001@gmail.com>
2023-04-28 20:47:38 -0500

    fixed front end work

--- assignment3/team28backend/dataSchema.js
+++ assignment3/team28backend/dataSchema.js
@@ -1,17 +1,22 @@
-const mongoose = require('mongoose');
-
-const ReactFormDataSchema = new mongoose.Schema({
-    _id: {type: Number},
-    title: {type: String},
-    price: {type: Number},
-    description: {type: String},
-    category: {type: String},
-    image: {type: String},
+const mongoose = require("mongoose");
+const ReactFormDataSchema = new mongoose.Schema(
+  {
+    _id: { type: Number },
+    title: { type: String },
+    price: { type: Number },
+    description: { type: String },
+    category: { type: String },
+    image: { type: String },
     rating: {
-        rate: {type: Number},
-        count: {type: Number}
-    }
-}, {collection: "fakestore_catalog"});
+      rate: { type: Number },
+      count: { type: Number },
+    },
+  },
+  { collection: "fakestore_catalog" }
+);
 
 const Product = mongoose.model('Product', ReactFormDataSchema);
-module.exports = Product;
+
+module.exports = {
+  Product,
+};
--- assignment3/team28backend/index.js
+++ assignment3/team28backend/index.js
@@ -1,93 +1,81 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const { async } = require("rxjs");
-const Product = require("./dataSchema");
+const { Product } = require("./dataSchema"); // import the Product model
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
 mongoose.connect("mongodb://127.0.0.1:27017/reactdata", {
-    dbName: "reactdata",
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+  dbName: "reactdata",
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
 const port = process.env.PORT || 4000;
 const host = "localhost";
-app.listen(port, () => {
-    console.log('App is listening at http://%s%s', host, port)
-});
 
-app.get("/", async(req, resp) => {
-    const query = {};
-    const allProducts = await Product.find(query);
-    console.log(allProducts);
-    resp.send(allProducts);
+app.get("/", async (req, resp) => {
+  const query = {};
+  const allProducts = await Product.find(query);
+  console.log(allProducts);
+  resp.set("Access-Control-Allow-Origin", "*");
+  resp.send(allProducts);
 });
 
 app.get("/:id", async (req, resp) => {
-    const id = req.params.id;
-    const query = {_id: id};
-    const oneProduct = await Product.findOne(query);
-    console.log(oneProduct);
-    resp.send(oneProduct);
+  const id = req.params.id;
+  const query = { _id: id };
+  const oneProduct = await Product.findOne(query);
+  console.log(oneProduct);
+  resp.send(oneProduct);
+});
+
+app.listen(port, () => {
+  console.log(`App listening at http://${host}:${port}`);
 });
 
 app.post("/insert", async (req, res) => {
-    console.log(req.body);
-    const p_id = req.body._id;
-    const ptitle = req.body.title;
-    const pprice = req.body.price;
-    const pdescription = req.body.description;
-    const pcatergory = req.body.category;
-    const pimage = req.body.image;
-    const prate = req.body.rating.rate;
-    const pcount = req.body.rating.count;
+  console.log(req.body);
+  const p_id = req.body._id;
+  const ptitle = req.body.title;
+  const pprice = req.body.price;
+  const pdescription = req.body.description;
+  const pcategory = req.body.category;
+  const pimage = req.body.image;
+  const prate = req.body.rating.rate;
+  const pcount = req.body.rating.count;
 
-    const formData = new Product({
-        _id: p_id,
-        title: ptitle,
-        price: pprice,
-        description: pdescription,
-        category: pcatergory,
-        image: pimage,
-        rating: {rate: prate, count: pcount},
-    });
-
-    try{
-        await Product.create(formData);
-        const messageRespone = {message: `Product ${p_id} added correctly`};
-        res.send(JSON.stringify(messageRespone));
-    } catch (err) {
-        console.log("Error while adding a new product: " + err);
-    }
+  const formData = new Product({
+    _id: p_id,
+    title: ptitle,
+    price: pprice,
+    description: pdescription,
+    category: pcategory,
+    image: pimage,
+    rating: { rate: prate, count: pcount },
+  });
+  try {
+    // await formData.save();
+    await Product.create(formData);
+    const messageResponse = { message: `Product ${p_id} added correctly` };
+    res.send(JSON.stringify(messageResponse));
+  } catch (err) {
+    console.log("Error while adding a new product:" + err);
+  }
 });
 
 app.delete("/delete", async (req, res) => {
-    console.log("Delete: ", req.body);
-    try {
-        const query = {_id: req.body._id};
-        await Product.deleteOne(query);
-        const messageRespone = {
-            message: `Product ${req.body._id} deleted correctly`
-        };
-        res.send(JSON.stringify(messageRespone));
-    } catch (err) {
-        console.log("Error while deleteing: " + p_id + " " + err);
-    }
-});
-
-app.put("/:id/updatePrice", async(req, res) => {
-    const id = req.params.id;
-    const newPrice = req.body.price;
-    try {
-        const query = { _id: id };
-        const update = { price: newPrice };
-        const updatedProduct = await Product.findOneAndUpdate(query, update, { new: true });
-        res.send(updatedProduct);
-    } catch (err) {
-        console.log("Error while updating product price: " + err);
-    }
+  console.log("Delete :", req.body);
+  try {
+    const query = { _id: req.body._id };
+    await Product.deleteOne(query);
+    const messageResponse = {
+      message: `Product ${req.body._id} deleted correctly`,
+    };
+    res.send(JSON.stringify(messageResponse));
+  } catch (err) {
+    console.log("Error while deleting :" + p_id + " " + err);
+  }
 });
